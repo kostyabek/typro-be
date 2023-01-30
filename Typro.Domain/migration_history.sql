71cf03def7d31055c060d730e3eb628d78a88327
kostyabek <kostyabek@gmail.com>
2023-01-30 19:11:08 +0200

    Added Users table migration

---
+++ Typro.Domain/migration_history.sql
@@ -0,0 +1,10 @@
+/*----- 30-01-2023 -----*/
+-- Create users table
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Users' and xtype='U')
+CREATE TABLE Users (
+    Id int IDENTITY(1, 1) NOT NULL,
+    Email varchar(255) NOT NULL UNIQUE,
+    PasswordHash varchar(255) NOT NULL,
+    PasswordSalt varchar(255) NOT NULL
+)
+GO
