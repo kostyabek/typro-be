b6b4bd6420ba99c558361dfc5714266cb77449d9
kostyabek <kostyabek@gmail.com>
2023-02-12 12:29:59 +0200

    Added user role getter to identity service

--- Typro.Application/Services/IUserIdentityService.cs
+++ Typro.Application/Services/IUserIdentityService.cs
@@ -1,6 +1,10 @@
-﻿namespace Typro.Application.Services;
+﻿using Typro.Domain.Enums;
+
+namespace Typro.Application.Services;
 
 public interface IUserIdentityService
 {
     public int UserId { get; }
+    public string UserEmail { get; }
+    public UserRole UserRole { get; }
 }
