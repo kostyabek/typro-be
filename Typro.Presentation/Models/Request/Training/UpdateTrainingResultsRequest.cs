b8987f8a2fc6fc5d20b0512ac4619b3a42c7204e
kostyabek <kostyabek@gmail.com>
2023-04-09 18:31:16 +0300

    Missing file

---
+++ Typro.Presentation/Models/Request/Training/UpdateTrainingResultsRequest.cs
@@ -0,0 +1,9 @@
+﻿using Typro.Domain.Models.Training;
+
+namespace Typro.Presentation.Models.Request.Training;
+
+public record UpdateTrainingResultsRequest(
+    float WordsPerMinute,
+    float Accuracy,
+    int TimeInMilliseconds,
+    CharacterStats CharactersStats);
